2fa3d878b1d94819fadbe7f5c57f6e8ba2a324f2
Ali Kefia <ali.kefia@gmail.com>
2021-11-26 18:08:50 +0100

    any deps

--- adf/node/activity.py
+++ adf/node/activity.py
@@ -24,8 +24,12 @@ class Activity(Node):
         self.depsKO = [
             d["activity"] for d in dependsOn if "Failed" in d["dependencyConditions"]
         ]
+        self.depsAny = [
+            d["activity"] for d in dependsOn if "Completed" in d["dependencyConditions"]
+        ]
         assert not (
-            set(d["activity"] for d in dependsOn) - set(self.depsOK + self.depsKO)
+            set(d["activity"] for d in dependsOn)
+            - set(self.depsOK + self.depsKO + self.depsAny)
         )
         self.init(**typeProperties)
         self.userProps = userProperties
@@ -40,6 +44,7 @@ class Activity(Node):
     def resolve(self):
         self.depsOK = [self.sibling(name) for name in self.depsOK]
         self.depsKO = [self.sibling(name) for name in self.depsKO]
+        self.depsAny = [self.sibling(name) for name in self.depsAny]
 
     def key(self):
         return f"{self.file}-{self.name}"
@@ -49,6 +54,8 @@ class Activity(Node):
             g.edge(dep.key_out(), self.key_in(), color="green")
         for dep in self.depsKO:
             g.edge(dep.key_out(), self.key_in(), color="red")
+        for dep in self.depsAny:
+            g.edge(dep.key_out(), self.key_in(), color="grey")
 
     def draw(self, g, **kwargs):
         super().draw(g, **kwargs)
@@ -59,7 +66,9 @@ class Activity(Node):
         head = set()
         tail = set(n.name for n in nodes)
         for node in nodes:
-            deps = set(d.name for d in (node.depsOK + node.depsKO)) & names
+            deps = (
+                set(d.name for d in (node.depsOK + node.depsKO + node.depsAny)) & names
+            )
             if not deps:
                 head.add(node.name)
             else:
